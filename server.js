8b90c8652d10d60af89def1d3cdd1ce1083ac267
Ratnesh Srivastava <ratsri17@gmail.com>
2020-06-20 17:59:49 +0530

    Allow origin added

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 const contact = require('./routes/contact-us');
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://advancetranscription.com"); 
+    res.header("Access-Control-Allow-Origin", ["http://advancetranscription.com", "http://www.advancetranscription.com"]); 
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
